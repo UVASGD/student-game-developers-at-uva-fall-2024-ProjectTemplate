e640bb48a4025601fa402f4e4d7b3bc95f9492d4
stick-h <68358227+stick-h@users.noreply.github.com>
2024-10-17 03:10:55 -0400

    Update settings.gd

--- source/scripts/settings.gd
+++ source/scripts/settings.gd
@@ -3,12 +3,10 @@ extends Control
 @onready var vbox = $TabContainer/Controls/MarginContainer/ScrollContainer/VBoxContainer
 
 var deviceIDs = []
-var old_connected_joypads
 
 func _ready():
 	for node in vbox.get_children():
 		if node.has_meta("player_num"): deviceIDs.append(node.get_meta("device_id"))
-		old_connected_joypads = Input.get_connected_joypads()
 
 func _process(delta):
 	if Input.is_action_just_pressed("esc"):
@@ -24,12 +22,6 @@ func _process(delta):
 			if Input.get_connected_joypads().has(i) && !deviceIDs.has(i):
 				deviceIDs[deviceIDs.find(4)] = i
 				break
-	
-	#if Input.get_connected_joypads() != old_connected_joypads:
-		#for i in range(4):
-			#if !(i in deviceIDs) && (i+1) in deviceIDs:
-				#deviceIDs[deviceIDs.find(i+1)] = i
-		#old_connected_joypads = Input.get_connected_joypads()
 	
 	for node in vbox.get_children():
 		if node.has_meta("player_num"):
