0508b30b551f46c8fa241e63185b8f3a2b34b495
Arti-Dev <boomcreeper296@gmail.com>
2024-10-26 14:05:34 -0400

    Change itemselector to only open when players stand in its portal

--- source/scenes/itemSelector/item_selector_portal.gd
+++ source/scenes/itemSelector/item_selector_portal.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var selectorScene: PackedScene = preload("res://source/scenes/itemSelector/item_selector.tscn")
+static var singleSelector: Item_Selector = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +13,15 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	if typeof(body) == typeof(Player):
-		var selector = selectorScene.instantiate()
-		get_tree().current_scene.add_child(selector)
+	if singleSelector != null: return
+	if body is Player:
+		singleSelector = selectorScene.instantiate()
+		singleSelector.setPlayer(body)
+		get_tree().current_scene.add_child(singleSelector)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if singleSelector == null: return
+	if body is Player and body == singleSelector.getPlayer():
+		singleSelector.free()
+		singleSelector = null
