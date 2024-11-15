2d063b42e234334241b4ae4116d8b7e942d16faa
Quintin Levy <159190598+quintinlevy@users.noreply.github.com>
2024-11-15 15:24:01 -0500

    added all player script parts

--- source/scripts/player.gd
+++ source/scripts/player.gd
@@ -23,6 +23,13 @@ enum Character {
 }
 var character = Character.PUMPKIN
 
+#Enemy attack instances
+#const Projectile_Scene := preload("res://source/scenes/projectile.tscn")
+#const Frank_Attack_Scene := preload("res://source/scenes/frankenstein_attack.tscn")
+#const Pumpkin_Attack_Scene := preload("res://source/scenes/pumpkin_attack.tscn")
+#const Ghost_Attack_Scene := preload("res://source/scenes/ghost_attack.tscn")
+
+
 var health :int = 0
 #signal onAttack(player)#This signals will emit every attack
 #signal onGetHit(player)#This signal will emit every time the player gets hit
@@ -59,6 +66,60 @@ func handle_move() -> void:
 		velocity.y = move_toward(velocity.y, 0, ACCELERATION)
 	
 	move_and_slide()
+
+func handle_attack(): #Right now, just enables, hitbox for 0.5 seconds
+	match character:
+		#THIS NEEDS TO BE UPDATED AFTER ATTACK SCENES MADE
+		Character.WITCH:
+			pass
+			#add_attack_instance_as_child(Projectile_Scene)
+		Character.FRANKENSTEIN:
+			pass
+			#add_attack_instance_as_child(Frank_Attack_Scene)
+		Character.GHOST:
+			pass
+			#add_attack_instance_as_child(Ghost_Attack_Scene)
+		Character.PUMPKIN:
+			pass
+			#add_attack_instance_as_child(Pumpkin_Attack_Scene)
+		_:
+			print("ERROR: Player not assigned character")
+	
+func handle_damage(attackingPlayer: CharacterBody2D) -> void:
+	pass
+	#UPDATE
+	#Health -= attackingPlayer.get_damage()
+	
+func shoot_projectile(projectile: PackedScene) -> void:
+	var proj_instance := projectile.instantiate()
+	# set the projectile instance at players locatio
+	proj_instance.position = self.global_position
+	# set direction of projectile towards mouse
+	proj_instance.direction = global_position.direction_to(get_global_mouse_position())
+	# assign damage from players stats to projectiles damage
+	#UPDATE
+	#proj_instance.set_damage(stats.attackDamage)
+	# attach attacking player to projectile
+	proj_instance.set_attackingPlayer(self)
+	#spawn projectile
+	add_child(proj_instance)
+	
+func add_attack_instance_as_child(attack_scene: PackedScene) -> void:
+	var attack_instance := attack_scene.instantiate()
+	attack_instance.position = self.global_position
+	attack_instance.direction = global_position.direction_to(get_global_mouse_position())
+	#UPDATE
+	#attack_instance.set_damage(stats.attackDamage)
+	attack_instance.set_attackingPlayer(self)
+	add_child(attack_instance)
+
+func getPlayerPosition() -> Vector2:
+	return position
+func get_damage() -> float:
+	return 0.0
+	#UPDATE
+	#return stats.attackDamage
+
 
 func get_item(item : Item):
 	damage += item.damage
