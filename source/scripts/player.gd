bbe4c25a5ad2233453d41dcef4eb11a7e1fe40d0
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2024-11-16 16:22:14 -0500

    Added statuseffects
    re implemented dashing
    fixed animation edge case
    added simple candy system

--- source/scripts/player.gd
+++ source/scripts/player.gd
@@ -52,7 +52,7 @@ const Ghost_Attack_Scene := preload("res://source/scenes/ghost_attack.tscn")
 
 
 var health : float = 0
-var candy : int
+var candy : int = 0
 var onAttackFunctions : Array[Callable]
 var onHitFunctions : Array[Callable]
 var onGetHitFunctions : Array[Callable]#When this one is called. should also call with the object hit as a parameter
@@ -78,12 +78,12 @@ func _process(delta: float) -> void:
 	if(movement != Vector2.ZERO): 
 		last_movement = movement
 
-func round_start():
+func round_start(): #called by game manager
 	call_functions(onRoundStart)
 
 func handle_move() -> void:
 	#movement = Vector2(Input.get_axis("Left" + player_num, "Right" + player_num), Input.get_axis("Up" + player_num, "Down" + player_num)).normalized()
-	playWalkOrIdleAnimation(velocity)
+	playWalkOrIdleAnimation()
 	if not velocity.is_zero_approx(): direction = velocity
 	
 	if movement.length() :
@@ -94,7 +94,7 @@ func handle_move() -> void:
 	movement = Vector2(Input.get_axis("Left" + player_num, "Right" + player_num), Input.get_axis("Up" + player_num, "Down" + player_num)).normalized() #this needs to use the input map
 	if not dash and Input.is_action_just_pressed("Dash"):
 		print("ENTERING DASH")
-		#dashing()
+		dashing()
 	if movement.length(): # stats.topSpeed = 10
 		Speed = move_toward(Speed, total_stats.speed, total_stats.speed * ACCELERATION)
 	else:
@@ -150,6 +150,20 @@ func handle_damage(attackingPlayer: CharacterBody2D) -> void:
 		i.call(self,attackingPlayer)
 	attackingPlayer = attackingPlayer as Player
 	attackingPlayer.call_functions(attackingPlayer.onHitFunctions)
+	if(attackingPlayer.isMonster):
+		match attackingPlayer.character:
+			Character.WITCH:
+				statusEffects.giveStatusTimed("Poison", max(3 * (1 - total_stats.tenacity * 0.1), 0), StatusEffectManager.OverLapBehavior.STACK)
+			Character.FRANKENSTEIN:
+				statusEffects.giveStatusTimed("Stun", max(3 * (1 - total_stats.tenacity * 0.1), 0))
+			Character.GHOST:
+				statusEffects.giveStatusTimed("Spook", max(3 * (1 - total_stats.tenacity * 0.1), 0))
+			Character.PUMPKIN:
+				statusEffects.giveStatusTimed("Fire", max(3 * (1 - total_stats.tenacity * 0.1), 0))
+			_:
+				print("ERROR: attacking Player does not have a valid character")
+	if(!isMonster && attackingPlayer.isMonster):
+		candy += 1
 	#UPDATE
 	#Health -= attackingPlayer.get_damage()
 	
@@ -166,9 +180,7 @@ func add_attack_instance_as_child(attack_scene: PackedScene) -> void:
 func getPlayerPosition() -> Vector2:
 	return position
 func get_damage() -> float:
-	return 0.0
-	#UPDATE
-	#return stats.attackDamage
+	return total_stats.attackDamage
 
 
 func get_item(item : Item):
@@ -200,8 +212,8 @@ func call_functions(arr : Array[Callable]):
 	for i in arr:
 		i.call()
 		
-func playWalkOrIdleAnimation(_velocity: Vector2):
-	if _velocity.is_zero_approx():
+func playWalkOrIdleAnimation():
+	if velocity.is_zero_approx():
 		sprite.play(model + "_idle_" + getDirectionWord(direction))
 	else:
 		sprite.play(model + "_walk_" + getDirectionWord(velocity))
@@ -209,10 +221,10 @@ func playWalkOrIdleAnimation(_velocity: Vector2):
 func getDirectionWord(_direction: Vector2):
 	if _direction.is_zero_approx(): return "down"
 	if abs(_direction.x) >= abs(direction.y):
-		if _direction.x > 0: return "right"
+		if _direction.x >= 0: return "right"
 		elif _direction.x < 0: return "left"
 	else:
-		if _direction.y > 0: return "down"
+		if _direction.y >= 0: return "down"
 		elif _direction.y < 0: return "up"
 
 #currently unused
@@ -268,10 +280,10 @@ func getDirectionWord(_direction: Vector2):
 	##add this in child Classes
 	#print("Player Attacked!")
 	#pass
-#func dashing():
+func dashing():
 	## dash values, please
-	#dash = true
-	#Speed = 750
-	#await get_tree().create_timer(2).timeout
-	#dash = false
-	##Speed = move_toward(Speed, 0, DECELERATION)
+	dash = true
+	Speed = 750
+	await get_tree().create_timer(2).timeout
+	dash = false
+	Speed = move_toward(Speed, 0, total_stats.speed * DECELERATION)
