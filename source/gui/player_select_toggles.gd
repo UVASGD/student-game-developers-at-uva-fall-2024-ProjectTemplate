71c92774291a91efc9855f86fa2ce70cb64a8c8a
kedrickfudala <KedrickAFudala@gmail.com>
2024-11-15 19:52:52 -0500

    character select background

--- source/gui/player_select_toggles.gd
+++ source/gui/player_select_toggles.gd
@@ -3,7 +3,11 @@ class_name PlayerSelectToggles
 
 @onready var sprite : Object = $Sprite2D
 @onready var value : int = 1
-@onready var sprite_textures = []
+@onready var pumpkin_texture = null
+@onready var frankenstein_texture = null
+@onready var witch_texture = null
+@onready var ghost_texture = null
+@onready var sprite_textures = [pumpkin_texture, frankenstein_texture, witch_texture, ghost_texture]
 
 func _physics_process(delta: float) -> void:
 	#sprite.texture = sprite_textures[value]
